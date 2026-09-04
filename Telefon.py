d0f041920d642d2fafea3ae8cd863b97a7da73ba
“SuperBrus99” <90walgab@stud.akademiet.no>
2026-09-04 15:22:38 +0200

    init

---
+++ Telefon.py
@@ -0,0 +1,15 @@
+telefonbok = []
+
+person1 = {
+    "navn": "Ola",
+    "nummer": "12345678"
+}
+
+person2 = {
+    "navn": "Kari",
+    "nummer": "87654321"
+}
+
+telefonbok.append(person1)
+telefonbok.append(person2)
+
